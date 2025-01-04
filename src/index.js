31488436f31daa9f807c6f3d3cd6ba2ddb39e1f2
Leuthra <romidev20@gmail.com>
2025-01-05 00:49:27 +0700

    fix: correct path for static files in Hono app

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { fileURLToPath } from "url";
 
 const app = new Hono();
 
-app.use("*", serveStatic({ root: "./public" }));
+app.use("*", serveStatic({ root: "public" }));
 
 // templates
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
