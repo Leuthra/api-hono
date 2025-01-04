c9b4bca32ef003a552980256e3467cbd811fa26a
Leuthra <romidev20@gmail.com>
2025-01-04 20:54:05 +0700

    refactor: replace axios with fetch in carbonara function for API call

--- src/scraper/maker/carbonara.js
+++ src/scraper/maker/carbonara.js
@@ -1,19 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
 async function carbonara(text) {
-    try {
-        const response = await axios.post('https://carbonara.solopov.dev/api/cook', {
-            code: text
-        }, {
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            responseType: 'arraybuffer'
-        });
-        return response.data;
-    } catch (error) {
-        throw new Error('Unable to generate image');
-    }
+  try {
+    const response = await fetch("https://carbonara.solopov.dev/api/cook", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        code: text,
+      }),
+    });
+    const buffer = await response.arrayBuffer();
+    return Buffer.from(buffer);
+  } catch (error) {
+    throw new Error("Unable to generate image");
+  }
 }
 
 export { carbonara };
